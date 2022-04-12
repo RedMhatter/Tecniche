c33004cc97be27984862db252c321cf571b4119d
RedMhatter <103261504+RedMhatter@users.noreply.github.com>
2022-04-12 17:24:06 +0200

    update es 2 lab 5

--- lab05/Esercizio 2/main.c
+++ lab05/Esercizio 2/main.c
@@ -11,11 +11,12 @@ typedef struct d
 
 int main() {
     FILE *fin, *dictionary, *fout;
-    char word[MAXL], dot;
+    char word[MAXL], temp[MAXL], dot;
     dizionario D[MAXL];
-    int i, j = 0;
+    int i, index_word = 0;
     int nr, found = 0;
 
+    //Apertura file
     fin = fopen("sorgente.txt", "r");
     dictionary = fopen("dizionario.txt", "r");
 
@@ -24,26 +25,51 @@ int main() {
     else
         fout = fopen("ricodificato.txt","w");
 
-
+    //Dictionary
     fscanf(dictionary, "%d", &nr);
     for (i=0; i<nr; i++)
         fscanf(dictionary, "%s%s", D[i].index1, D[i].index2);
     fclose(dictionary);
 
+    //Algoritmo
     while(fscanf(fin, "%s%c", word, &dot) >= 1)
     {
         for (i=0; i<nr && found == 0; i++)
-        {
             if(strstr(word, D[i].index2) != NULL)
             {
-                fprintf(fout, "%s%c", D[i].index1, dot);
+                index_word = i;
                 found = 1;
             }
-            /*else
-                //fprintf(fout, "%")*/
-        }
 
+        if (found == 0)
+            fprintf(fout, "%s", word, dot);
+        else
+        {
+            if(strlen(word) - strlen(D[index_word].index2) > 0)
+            {
+                for (i=0; i < strlen(word); i++)
+                {
+                    strncpy(temp, word+i, strlen(D[index_word].index2));
+                    printf("%s %d", temp, strlen(word));
+
+                    if(strcmp(temp, D[index_word].index2) != 0)
+                        fprintf(fout, "%c", word[i]);
+                    else
+                    {
+                        fprintf(fout, "%s", D[index_word].index1);
+                        i += strlen(D[index_word].index2) - 1;
+                    }
+
+                    memset(temp,0,strlen(temp)); //Devo cercare di cambiare questo comando
+                }
+            }
+            else
+                fprintf(fout, "%s", D[index_word].index1);
+        }
         found = 0;
+        fprintf(fout, "%c", dot);
+        index_word = 0;
     }
+    fclose(fin), fclose(fout);
     return 0;
 }
